48bd653e8ae6cdfdd7d2f0bd66b5f9fbe3c9a7e3
abdulloh0099 <abdulhamidov118@gmail.com>
2026-04-20 22:00:10 +0500

    about me to'liq yakunlandi

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import React from 'react'
-import "./App.css"
-import headerlogo from "./assets/header-logo.png"
-import avatar from "./assets/Gleb.png"
-import hometitle from "./assets/title.png"
-import aboutimgs from "./assets/Sills.png"
+import React from 'react';
+import './App.css';
+import headerlogo from './assets/header-logo.png';
+import avatar from './assets/Gleb.png';
+import hometitle from './assets/title.png';
+import aboutimgs from './assets/Sills.png';
+import group3 from './assets/group3.png';
 const App = () => {
   return (
     <div>
       <header>
-          <group-1 className="header-logo">
-            <img src={headerlogo} className="header-img" />
-            <h1 className="header-h1">notitanic</h1>
-          </group-1>
-          <group-2 className="pages">
-            <h1 className="page-h1">Home</h1>
-            <h1 className="page-h1">About me</h1>
-            <h1 className="page-h1">Portfolio</h1>
-            <h1 className="page-h1">Contact</h1>
-          </group-2>
+        <group-1 className="header-logo">
+          <img src={headerlogo} className="header-img" />
+          <h1 className="header-h1">notitanic</h1>
+        </group-1>
+        <group-2 className="pages">
+          <h1 className="page-h1">Home</h1>
+          <h1 className="page-h1">About me</h1>
+          <h1 className="page-h1">Portfolio</h1>
+          <h1 className="page-h1">Contact</h1>
+        </group-2>
       </header>
       <home-page>
         <img src={hometitle} className="home-title" />
         <img src={avatar} className="home-avatar" />
       </home-page>
-      <div className='about-me'>
+      <div className="about-me">
         <img src={aboutimgs} className="aboutimgs" />
+        <img src={group3} alt="" className="group3" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
